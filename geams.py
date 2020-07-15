fc742f94d9c1215084b9bf5953e98831c3b97de3
Yasser Bader <67002064+1065196118@users.noreply.github.com>
2020-07-15 20:53:31 +0200

    Add files via upload

---
+++ geams.py
@@ -0,0 +1,12 @@
+from rr import *
+print("Theoretical questions enter(e) and practical enter(p)")
+cha=input("enter your change?= ")
+
+if cha=='p':
+    amly()
+else:
+    print("Choose the correct answer (select multiple)")
+    print(''' (multipl choice)what are the commonly used activion functions?''')
+    print('''a/ sigmoid            b/ tanh
+c/relu                d/ danish''')
+    tt()
